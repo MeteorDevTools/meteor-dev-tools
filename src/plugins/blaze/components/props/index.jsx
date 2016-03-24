c21b64b79a07ec6a4570551a70fd0f48c7b0a75b
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-03-24 16:00:54 +0000

    clean up

--- src/plugins/blaze/components/props/index.jsx
+++ src/plugins/blaze/components/props/index.jsx
@@ -1,20 +1,15 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-import _ from 'underscore';
 import JSONTree from 'react-json-tree';
 
 export default React.createClass({
   render () {
     if(this.props.properties) {
       let getArrowStyle = (type, expanded) => ({ marginTop: 4 });
-      console.error('selected props are', this.props.properties);
-
       const data = this.props.properties.data;
       const events = this.props.properties.events;
       const helpers = this.props.properties.helpers;
       
-      console.error('@@@@@ data is', data);
-
       return <div>
         { data && data.size !== 0 ? (
           <div>
